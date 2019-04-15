413aa1cc64515d6d41eea19bc99174234acb54e0
Augusto Valdivia <augustovaldivia@live.com>
2019-04-15 14:36:18 -0400

    test.py updated

--- web/tests.py
+++ web/tests.py
@@ -10,7 +10,7 @@ class MainTestCase(unittest.TestCase):
         self.assertNotEqual(four, 22)
         self.assertNotEqual(four, 102)
         self.assertNotEqual(four, 60)
-        self.assertNotEqual(four, 50)
+        
       
         
 
